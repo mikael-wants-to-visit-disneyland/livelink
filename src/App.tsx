1f9a639d6fcc3864733fb7dd508ed39f8837460e
Mikael <mikael@Mikaels-MBP.lan>
2022-08-27 14:15:29 +0100

    extracted some css to the file

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import cardDetails from "./dummyData.json";
-import UserCard, { IUserCardProps } from "./components/UserCard";
+import UserCard from "./components/UserCard";
 
 function App() {
   const [activeCard, setActiveCard] = React.useState<number | null>(null);
   return (
     <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        background: "#FDEFE6",
-        paddingTop: "50px",
-      }}
+      className="usercard-view"
       onClick={() => activeCard !== null && setActiveCard(null)}
     >
-      <img src={logo} className="logo" alt="logo" />
-      <div style={{ display: "flex", flexDirection: "column", width: "77%" }}>
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <div className="usercard-list">
         {cardDetails.map((details, i) => (
           <div key={i} onClick={() => setActiveCard(i)}>
             <UserCard
